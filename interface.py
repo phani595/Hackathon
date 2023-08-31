4c43ffbbd7e6dcd87e69bbbffff753c3539c1105
Kumar Thota <phani595@gmail.com>
2023-08-31 13:48:59 +1000

    feat: commit

---
+++ interface.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import pandas as pd
+import json
+from agent import query_agent, create_agent
+
+import pandas as pd
+from pandasai import PandasAI
+
+import numpy as np
+
+
+def decode_response(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+
+def write_response(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+    st.write(response_dict)
+    st.write(np.random.randn(5, 2))
+    # Check if the response is an answer.
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    # Check if the response is a bar chart.
+    if "bar" in response_dict:
+        # data = response_dict["bar"]
+        # df = pd.DataFrame(data)
+        # df.set_index("columns", inplace=True)
+        # st.bar_chart(df)
+        # chart_data = pd.DataFrame(
+        #     np.random.randn(20, 2),
+        #     columns=["a", "b"])
+        chart_data = pd.DataFrame(
+            [{1,3}, {2,4}],
+            columns=["a", "b"])
+        st.bar_chart(chart_data)
+        
+
+    # Check if the response is a line chart.
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+
+    # Check if the response is a table.
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
+
+
+st.title("👨‍💻 I can help you ")
+
+st.write("Please upload your CSV file below.")
+
+data = st.file_uploader("Upload a CSV")
+
+query = st.text_area("Insert your query")
+
+if st.button("Submit Query", type="primary"):
+    # Create an agent from the CSV file.
+    # agent = create_agent(data)
+    df = pd.read_csv(data) 
+    data_frames = []
+    data_frames.append(df)
+
+    combined_df = pd.concat(data_frames, ignore_index=True)
+
+    from pandasai.llm.openai import OpenAI
+    llm = OpenAI(api_token="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")
+
+    pandas_ai = PandasAI(llm, conversational=False, enable_cache=True)
+
+    response = pandas_ai.run(combined_df, prompt=query)
+
+   
+
+    st.write(response)
+
+    # Query the agent.
+    # response = query_agent(agent=agent, query=query)
+
+    # Decode the response.
+    # decoded_response = decode_response(response)
+
+    # Write the response to the Streamlit app.
+    # write_response(decoded_response)
